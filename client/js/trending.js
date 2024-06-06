7c75a8349e89574d4d849761dc84ae253aa55d18
Moloce Alin <alinmoloce212@gmail.com>
2024-06-06 09:35:43 +0300

    improved page speed performance score

--- client/js/trending.js
+++ client/js/trending.js
@@ -3,39 +3,58 @@ const jsonElements = [];
 fetch('http://localhost:5010/api/resources/latestResources')
     .then(response => response.json())
     .then(data => {
-
-        var rows = "";
-
+        let rows = "";
         data.forEach((element, index) => {
             jsonElements.push(element);
-
-            rows += '<article '
-                + 'onclick="updatePreview(' + index + ')">'
-                + '<h2>' + element.title + '</h2>'
-                + '<p>' + element.summary + '</p>'
-                + '<p class="tags">' + element.tags.split(',').map(tag => '#' + tag.trim().replace(' ', '_')) + '</p>'
-                + "</article>"
+            rows += `<article onclick="updatePreview(${index})">
+                        <h2>${element.title}</h2>
+                        <p>${element.summary}</p>
+                        <p class="tags">${element.tags.split(',').map(tag => '#' + tag.trim().replace(' ', '_')).join(', ')}</p>
+                     </article>`;
         });
-
         document.getElementById("fetchedResources").innerHTML = rows;
-
     });
 
-
 function updatePreview(index) {
-    var element = jsonElements[index];
+    const element = jsonElements[index];
+    const html = `<article>
+                    <h2>${element.title}</h2>
+                    <p>${element.description}</p>
+                    <h2>Resource link:</h2>
+                    <a href="${element.link}">${element.link}</a>
+                    <br><h2>Read more:</h2>
+                    <a href="product.html?id=${element.id}">${element.title}</a>
+                  </article>`;
+    document.getElementById("articlePreview").innerHTML = html;
+}
 
+function checkToken(token) {
+    return new Promise((resolve, reject) => {
+        if (!token) {
+            resolve(false);
+            return;
+        }
+        fetch('http://localhost:5010/api/users/authentification', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ token }),
+            credentials: 'include'
+        }).then(res => {
+            resolve(res.status !== 404 && res.status !== 500);
+        }).catch(() => {
+            resolve(false);
+        });
+    });
+}
 
-    var articlePreview = document.getElementById("articlePreview");
-
-    var html  = '<article>'
-                +'<h2>' + element.title + '</h2>'
-                + '<p>' + element.description + '</p>'
-                + '<h2>Resource link:</h2>'
-                + '<a href="' + element.link + '">' + element.link + '</a>'
-                + '<br><h2>Read more:</h2>'
-                + '<a href="' + "product.html?id=" + element.id + '">'+element.title+'</a>'
-                + '</article>';
-    articlePreview.innerHTML = html;
-
+function logoRedirect() {
+    const jwtToken = localStorage.getItem('token');
+    checkToken(jwtToken).then(isValid => {
+        if (isValid) {
+            window.location.href = "main-admin.html";
+        } else {
+            localStorage.removeItem('token');
+            window.location.href = "main-guest.html";
+        }
+    });
 }
